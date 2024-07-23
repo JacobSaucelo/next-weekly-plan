b111ec6b241b07f45b43c98361127777a07858c4
Saucelo <jacobsaucelo32@gmail.com>
2024-07-23 15:25:54 +0800

    Added pagination

--- app/tools/todo/page.tsx
+++ app/tools/todo/page.tsx
@@ -39,6 +39,66 @@ async function getData(): Promise<Payment[]> {
       status: "pending",
       email: "5dcs324@example.com",
     },
+    {
+      id: "vded3rd",
+      amount: 360,
+      status: "pending",
+      email: "cdhtg4@example.com",
+    },
+    {
+      id: "nyefv4",
+      amount: 90,
+      status: "failed",
+      email: "fdadss@example.com",
+    },
+    {
+      id: "g4refdv",
+      amount: 270,
+      status: "success",
+      email: "vr4ead@example.com",
+    },
+    {
+      id: "y645fgr",
+      amount: 21,
+      status: "processing",
+      email: "hyb6t@example.com",
+    },
+    {
+      id: "dvc4te",
+      amount: 30,
+      status: "pending",
+      email: "judfv5d@example.com",
+    },
+    {
+      id: "er3e3df",
+      amount: 1030,
+      status: "pending",
+      email: "74fdads@example.com",
+    },
+    {
+      id: "nyfgf",
+      amount: 70,
+      status: "failed",
+      email: "nut45dc@example.com",
+    },
+    {
+      id: "cdsfd",
+      amount: 230,
+      status: "success",
+      email: "gfsdcd@example.com",
+    },
+    {
+      id: "deasdfasd",
+      amount: 2541,
+      status: "processing",
+      email: "deasdfasd@example.com",
+    },
+    {
+      id: "tew34",
+      amount: 60,
+      status: "pending",
+      email: "tew34@example.com",
+    },
   ];
 }
 
