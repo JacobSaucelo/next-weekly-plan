74d4bce7b390e73d6590ce59a64b9df46af9ed8b
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-08-04 12:40:27 +0800

    added NextTopLoader

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import NextTopLoader from "nextjs-toploader";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
