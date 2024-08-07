5928ce59ea3023f868707e14fdd1b83f07d852ec
Saucelo <jacobsaucelo32@gmail.com>
2024-08-07 16:03:19 +0800

    added alert dialog functionality for done and remove

--- components/Customs/TodoSubTask.Card.tsx
+++ components/Customs/TodoSubTask.Card.tsx
@@ -8,7 +8,19 @@ import {
   CircleX,
   LayoutList,
 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import ReusablesDivider from "../Reusables/Reusables.Divider";
+import { Badge } from "../ui/badge";
 
 type SubTaskCardPropsType = {
   props: SubTaskType;
@@ -37,6 +49,8 @@ const TodoSubTaskCard = ({ props, depth }: SubTaskCardPropsType) => {
     return;
   }
 
+  const handleDoneTask = () => {};
+
   return (
     <aside className="border-b-2 border-gray-400 flex py-1 px-2 items-center justify-between gap-2">
       {props.isDone ? (
@@ -53,20 +67,64 @@ const TodoSubTaskCard = ({ props, depth }: SubTaskCardPropsType) => {
       <p>{props.isDeleted ? "true" : "false"}</p> */}
 
       <div className="flex gap-2 ps-2">
-        <Button size="sm" variant="outline" className="flex gap-1">
-          <CircleCheck className="text-green-400" />
-          Done
-        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <Button size="sm" variant="outline" className="flex gap-1">
+              <CircleCheck className="text-green-400" />
+              Done
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure you want to complete this task?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                Confirm to proceed with marking this task named
+                <Badge className="mx-1">&quot;{props.name}&quot;</Badge>as
+                complete.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button className="flex gap-1">
+                <CircleCheck /> Complete Subtask
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+
         {depth < 3 && (
           <Button size="sm" variant="outline" className="flex gap-1 ">
             <CirclePlus className="text-blue-400" />
             Add
           </Button>
         )}
-        <Button size="sm" variant="outline" className="flex gap-1 ">
-          <CircleX className="text-red-600" />
-          Remove
-        </Button>
+
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <Button size="sm" variant="outline" className="flex gap-1 ">
+              <CircleX className="text-red-600" />
+              Remove
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Once you proceed, you won’t be able to reverse this action. It
+                will permanently remove your subtask named{" "}
+                <Badge>&quot;{props.name}&quot;</Badge>.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button className="flex gap-1" variant="destructive">
+                <CircleX /> Delete Subtask
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </aside>
   );
